0fd05a18c88ddc0d2d4ce84b55043e652d44ffe8
aneylon <arlen.m.neylon@gmail.com>
2026-09-24 08:24:19 -0600

    Add example.

---
+++ examples/02/main.js
@@ -0,0 +1,28 @@
+console.log("main!");
+
+function changeBackgroundColor() {
+  document.body.style.background = "blue";
+}
+
+var canvas = undefined;
+var canvasContext = undefined;
+
+function start() {
+  canvas = document.getElementById("mycanvas");
+  canvasContext = canvas.getContext("2d");
+  mainLoop();
+}
+
+document.addEventListener("DOMContentLoaded", start);
+
+function update() {}
+
+function draw() {}
+
+function mainLoop() {
+  canvasContext.fillStyle = "blue";
+  canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+  update();
+  draw();
+  window.setTimeout(mainLoop, 1000 / 60);
+}
